ecf8cc7688434da799405ee6c41fd9fb6aa9ed67
Dioprz <diospinar@unal.edu.co>
2022-05-25 17:41:24 -0500

    Prueba 1

---
+++ src/main/java/futbol/Jugador.java
@@ -0,0 +1,33 @@
+package futbol;
+
+public class Jugador extends Futbolista{
+
+    public short golesMarcados;
+    public byte dorsal;
+
+    public Jugador(String nombre, int edad, String posicion, short golesMarcados, byte dorsal){
+        super(nombre, edad, posicion);
+        this.golesMarcados = golesMarcados;
+        this.dorsal = dorsal;
+    }
+
+    public Jugador(){
+        //super() lo pone el compilador
+        this.golesMarcados = 289;
+        this.dorsal = 7;
+    }
+
+    public int compareTo(Futbolista futbolista){
+        return Math.abs(this.edad - futbolista.getEdad());
+    }
+
+    public boolean jugarConLasManos(){
+        return False;
+    }
+
+    public String toString(){
+        return "El futbolista " + nombre + " tiene " + edad + " y juega de " posicion " con el dorsal " + dorsal + ". Ha marcado " + goles;
+    }
+
+}
+
---
+++ src/main/java/futbol/Portero.java
@@ -0,0 +1,27 @@
+package futbol;
+
+public class Portero extends Futbolista{
+
+    public short golesRecibidos;
+    public byte dorsal;
+
+    public Portero(String nombre, int edad, String posicion, short golesRecibidos, byte dorsal){
+        super(nombre, edad, "Portero");
+        this.golesRecibidos = golesRecibidos;
+        this.dorsal = dorsal;
+    }
+     
+    public String toString(){
+        return "El futbolista " + nombre + " tiene " + edad + " y juega de " posicion " con el dorsal " + dorsal + ". Le han marcado " + goles;
+    }
+
+    public boolean jugarConLasManos(){
+        return True;
+    }
+
+    public int compareTo(Portero portero){
+        return Math.abs(this.golesRecibidos - portero.golesRecibidos());
+    }
+
+}
+
